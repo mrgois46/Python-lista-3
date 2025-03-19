ac264159059c78b7afd5c870ec5358bc8fa7c9c7
gois murilo <murilo1997mendes@gmail.com>
2025-03-19 19:56:06 -0300

    Add files via upload

---
+++ ex10.py.py
@@ -0,0 +1,13 @@
+a1 = int(input("Digite um valor :"))
+
+if a1<10 :
+    print(a1*1)
+    print(a1*2)
+    print(a1*3)
+    print(a1*4)
+    print(a1*5)
+    print(a1*6)
+    print(a1*7)
+    print(a1*8)
+    print(a1*9)
+    print(a1*10)
